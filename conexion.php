34b6b02d84c11fee27ed9137d687a6a1e40fecc8
Neburfz <59911237+Neburfz@users.noreply.github.com>
2020-01-17 13:52:56 +0100

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php
 /* Conexión BD */
-define('DB_SERVER', '10.128.12.68');
+define('DB_SERVER', '10.128.11.187');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'rootroot');
-define('DB_DATABASE', 'COMPRASWEB');
+define('DB_DATABASE', 'comprasweb');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
    if (!$db) {
